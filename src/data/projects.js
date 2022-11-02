e7438c649b6921ab363d4612de75c1309d744ea7
Gilan <serrantg@users-MacBook-Pro.local>
2022-11-01 22:16:10 -0400

    projects updated

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,47 +1,25 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
-    desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+    title: 'Digital Dash',
+    link: 'https://gitlab.com/gjkk/amazing-project',
+    image: '/images/projects/digitaldash.jpeg',
+    date: '2022-08',
+    desc: "Network performance management website that includes a network speed test and a downloadable bandwidth monitor that keeps track of users' data usage. ",
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
-    desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
+    title: 'Car Management',
+    link: 'https://gitlab.com/GTech_/car-management',
+    image: '/images/projects/carmanage.jpeg',
+    date: '2022-07-20',
+    desc: 'full-stack web application that enabled functions to include employee tracking, workflow management, and sales reporting, for a car dealership.',
   },
   {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
-    link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
-    desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
-  },
-  {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+    title: 'Conference Go',
+    link: 'https://gitlab.com/GTech_/fearless-front-end-gs',
+    image: '/images/projects/conferencego.jpeg',
+    date: '2022-06',
+    desc: 'Web application using Django that enabled attendee management for various conferences in the United States',
   },
 ];
 
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,15 +6,19 @@ import Main from '../layouts/Main';
 import Education from '../components/Resume/Education';
 import Experience from '../components/Resume/Experience';
 import Skills from '../components/Resume/Skills';
-import Courses from '../components/Resume/Courses';
 import References from '../components/Resume/References';
 
-import courses from '../data/resume/courses';
 import degrees from '../data/resume/degrees';
 import positions from '../data/resume/positions';
 import { skills, categories } from '../data/resume/skills';
 
-const sections = ['Education', 'Experience', 'Skills', 'Courses', 'References'];
+const sections = [
+  'Education',
+  'Experience',
+  'Skills',
+  'Projects',
+  'References',
+];
 
 const Resume = () => (
   <Main
@@ -39,7 +43,6 @@ const Resume = () => (
       <Education data={degrees} />
       <Experience data={positions} />
       <Skills skills={skills} categories={categories} />
-      <Courses data={courses} />
       <References />
     </article>
   </Main>
